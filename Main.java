2e644c992cd0f058f4824678cd21c2c59315fb52
Igor <igorvgoncharov@mail.ru>
2023-06-22 22:03:25 +0300

    BinaryTree added

--- Main.java
+++ Main.java
@@ -1,19 +1,29 @@
 ﻿public class Main {
     public static void main(String[] args) {
 
-        LinkedList Nodelist = new LinkedList();
+        //LinkedList Nodelist = new LinkedList();
 
-        Nodelist.add(5);
-        Nodelist.add(4);
-        Nodelist.add(6);
-        Nodelist.add(1);
-        Nodelist.add(8);
-        Nodelist.add(7);
-        Nodelist.add(9);
+        //Nodelist.add(5);
+        //Nodelist.add(4);
+        //Nodelist.add(6);
+        //Nodelist.add(1);
+        //Nodelist.add(8);
+        //Nodelist.add(7);
+        //Nodelist.add(9);
 
-        Nodelist.print();
-        Nodelist.revert();
-        Nodelist.print();
+        //Nodelist.print();
+        //Nodelist.revert();
+        //Nodelist.print();
+
+        BinaryTree<Integer> tree = new BinaryTree<>();
+        tree.add(6);
+        tree.add(3);
+        tree.add(2);
+        tree.add(4);
+
+        tree.remove(6);
+
+        tree.print();
 
     }
 }
